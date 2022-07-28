a2489453fb8eaeb21ce859cd0074891517d33c97
yanot <0tp6324w3261z0v@gmail.com>
2022-07-28 15:14:01 +0900

    🔥 test

--- .eslintrc.js
+++ .eslintrc.js
@@ -77,9 +77,5 @@ module.exports = {
         ],
       },
     },
-    {
-      files: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[jt]s?(x)"],
-      extends: ["plugin:testing-library/react"],
-    },
   ],
 };
